cf7abd48c7e33e65515d1dd00cc8efd88cb56ba1
Vishnuvardani <vishnu@gmail.com>
2026-07-08 11:20:36 +0530

    Added input and total calculation

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
-#include <stdio.h>
+#include<stdio.h>
+
 int main()
 {
-    printf("Hello World");
+    int m1,m2,m3,total;
+    float avg;
+    char grade;
+
+    printf("Enter three marks: ");
+    scanf("%d%d%d",&m1,&m2,&m3);
+
+    total=m1+m2+m3;
+    avg=total/3.0;
+
+    if(avg>=90)
+        grade='A';
+    else if(avg>=75)
+        grade='B';
+    else if(avg>=60)
+        grade='C';
+    else if(avg>=50)
+        grade='D';
+    else
+        grade='F';
+
+    printf("Total = %d\n",total);
+    printf("Average = %.2f\n",avg);
+    printf("Grade = %c\n",grade);
+
+    return 0;
 }
